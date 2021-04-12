47e2bdc855505ffbf9a6c56ccc45b773be82feb2
Philip Harrison <philip@mailharrison.com>
2021-04-12 12:27:55 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -98,16 +98,6 @@ resource "aws_cloudfront_distribution" "default" {
   tags = "${module.distribution_label.tags}"
 }
 
-module "dns" {
-  source           = "git::https://github.com/cloudposse/terraform-aws-route53-al.git//some/dir?ref=tags/0.2.5"
-  enabled          = "${var.dns_aliases_enabled}"
-  aliases          = "${var.aliases}"
-  parent_zone_id   = "${var.parent_zone_id}"
-  parent_zone_name = "${var.parent_zone_name}"
-  target_dns_name  = "${aws_cloudfront_distribution.default.domain_name}"
-  target_zone_id   = "${aws_cloudfront_distribution.default.hosted_zone_id}"
-}
-
 
 module "github_ssh_without_protocol" {
   source     = "git@github.com:cloudposse/terraform-aws-jenkins.git?ref=tags/0.4.0//some/dir"
